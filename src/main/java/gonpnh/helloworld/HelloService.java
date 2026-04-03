db16c9d1453ffc5e3959b03828315a6d1ca6f798
pinho <pinhogoncalofr@gmail.com>
2026-04-03 15:09:00 +0200

    Création

---
+++ src/main/java/gonpnh/helloworld/HelloService.java
@@ -0,0 +1,13 @@
+package gonpnh.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloService {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
